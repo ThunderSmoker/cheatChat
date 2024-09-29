955db02d6caadfd368810ad971dcf1b28290a4aa
ThunderSmoker <pitlawararadhya@gmail.com>
2024-09-29 16:45:43 +0530

    bug fix

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -5,7 +5,7 @@ import Message from '../../models/Message'; // Create a message model
 // Connect to MongoDB
 const connectDB = async () => {
   if (mongoose.connection.readyState >= 1) return;
-  await mongoose.connect(process.env.MONGODB_URI? process.env.MONGODB_URI : '');
+  await mongoose.connect(process.env.MONGODB_URI? process.env.MONGODB_URI : 'mongodb://localhost:27017');
 };
 
 export async function POST(request: Request) {
