8471f952756c5eeab1fec667dbce19325e7aaf58
ThunderSmoker <pitlawararadhya@gmail.com>
2024-09-29 18:52:32 +0530

    bug fix

--- src/app/component/Chat.tsx
+++ src/app/component/Chat.tsx
@@ -137,6 +137,7 @@ const Chat = () => {
               <a
                 href={msg.file}
                 download={msg.text.split(' ')[0]}
+                target='_blank'
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2 inline-block"
               >
                 Download File
